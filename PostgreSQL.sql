afe659e8f425e7fdb29404e77b7e703f13923dbb
Gabriel0070 <161778562+Gabriel0070@users.noreply.github.com>
2024-08-15 21:17:07 -0400

    Add files via upload

---
+++ PostgreSQL.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE MeuDb
+
+
+CREATE TABLE usuario(
+  id_usuario integer PRIMARY KEY NOT NULL,
+  nome VARCHAR(100)
+  idade INTEGER
+  );
+  
+  CREATE TABLE aluno(
+  id_aluno INTEGER PRIMARY KEY NOT NULL,
+  nome VARCHAR(100),
+  idade INTEGER,
+  ano INTEGER,
+  periodo INTEGER (25)
+  );
